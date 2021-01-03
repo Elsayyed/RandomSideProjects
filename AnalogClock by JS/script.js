eccfa95c6a377382b9747383ebbca94aa25d9f95
Mohammad Elsayyed <upmohammad@gmail.com>
2021-01-03 17:40:43 -0500

    Final Version of the Analog Clock

--- AnalogClock by JS/script.js
+++ AnalogClock by JS/script.js
@@ -2,10 +2,21 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hourArmPosition = 20;
-let minutesArmPosition = 130;
-let secondArmPosition = 267;
+function clockFace() {
+	var date = new Date();
+	console.log(date);
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let seconds = date.getSeconds();
+	console.log(hours + " " + minutes + " " + seconds );
 
-HOURHAND.style.transform = "rotate(" + hourArmPosition+ "deg)";
-MINUTEHAND.style.transform = "rotate(" + hourArmPosition+ "deg)";
-SECONDHAND.style.transform = "rotate(" + hourArmPosition+ "deg)";
+	let hourArmPosition = (hours*360/12) + ((minutes*(360/60))/12);
+	let minutesArmPosition = (minutes*360/60) + (seconds*(360/60)/60);
+	let secondArmPosition = seconds*(360/60);
+
+	HOURHAND.style.transform = "rotate(" + hourArmPosition+ "deg)" ;
+	MINUTEHAND.style.transform = "rotate(" + minutesArmPosition+ "deg)" ;
+	SECONDHAND.style.transform = "rotate(" + secondArmPosition+ "deg)" ;
+}
+
+var clockRefreshInterval = setInterval(clockFace,1000);
